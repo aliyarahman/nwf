795c08a021bc68fd201f9240cf57eda824bf51a7
aliyarahman <aliya.rahman@yahoo.com>
2015-03-29 14:48:02 -0400

    Updated Chapter 1 layout

--- js/application.js
+++ js/application.js
@@ -1,31 +1,45 @@
-  jQuery(function($) {
-      
+$(document).ready(function() {
+
+
+  //PanelSnap options and function call
   var options = {
-  $menu: false,
-  menuSelector: 'a',
-  panelSelector: '> section',
-  namespace: '.panelSnap',
-  onSnapStart: function(){},
-  onSnapFinish: function(){},
-  onActivate: function(){},
-  directionThreshold: 50,
-  slideSpeed: 200,
-  easing: 'linear',
-  offset: 0,
-  keyboardNavigation: {
-    enabled: true,
-    nextPanelKey: 40,
-    previousPanelKey: 38,
-    wrapAround: true
-  }
-};  
+    $menu: false,
+    menuSelector: 'a',
+    panelSelector: '> section',
+    namespace: '.panelSnap',
+    onSnapStart: function(){},
+    onSnapFinish: function(){},
+    onActivate: function(){},
+    directionThreshold: 50,
+    slideSpeed: 200,
+    easing: 'linear',
+    offset: 0,
+    keyboardNavigation: {
+      enabled: true,
+      nextPanelKey: 40,
+      previousPanelKey: 38,
+      wrapAround: true
+    }
+  };  
+
+//  $('body').panelSnap();
 
 
-      $('body').panelSnap();
+  //Global behavior
+
+  $('.chapter-icon').hover(function() {
+      $('.chapter-details').slideDown();
+  });
+
+
+  //Chapter 3 behavior
 
 
 
-
-
-
-      });
+  $("#ch3-full-image").mouseover(function() { 
+        $(this).attr("src", "img/Ch3-hover.jpg");
+        })
+        .mouseout(function() {
+        $(this).attr("src", "img/Ch3.jpg");
+        });
+});
