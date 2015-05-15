949bbd322767938112c35dd8656bee752daacc3c
aliyarahman <aliya.rahman@yahoo.com>
2015-05-14 22:37:39 -0400

    Updated full mobile layout minus menu bar and chapter 2 maps

--- js/adaptive.js
+++ js/adaptive.js
@@ -23,21 +23,22 @@ $(document).ready(function() {
         if ($(window).width() > 768) {
             window.location = "index.html";
         }
-        $(window).scrollTop() = y_scroll_pos;
     });
 
     // Code to control main text display for each chapter
 
     $('.read-more-button').on('click', function(){
-        $(this).hide();
+        $(this).parent().hide();
         $(this).parent().parent().parent().find('.chapter-main-text').slideDown();
-        $(this).parent().parent().parent().find('.read-less-button').show();        
+        $(this).parent().parent().parent().find('.read-less-button').show();
     });
 
     $('.read-less-button').on('click', function(){
         $(this).parent().parent().parent().find('.chapter-main-text').slideUp();
+        $('.read-more-less').show();
         $('.read-more-button').show();  
         $(this).hide();
+
     });
 
 
@@ -57,36 +58,6 @@ $(document).ready(function() {
         $('.tangent-footer').load('../footer.html');
     });
 
-
-
-/*
-    // Chapter Scrolling behavior
-    // ====================================
-    $(window).on('scroll', function() {
-        
-        var y_scroll_pos = window.pageYOffset;
-        var sec_2_y = $('#chapter-two').offset().top;
-        var sec_3_y = $('#chapter-three').offset().top;
-        var sec_4_y = $('#chapter-four').offset().top;
-        var sec_5_y = $('#chapter-five').offset().top;
-
-        if (y_scroll_pos > sec_2_y) {
-            // Chapter 2
-            console.log("we've reached chapter two");
-        }
-        else if (y_scroll_pos > sec_3_y) {
-            console.log("we've reached chapter three");
-        }
-        else if (y_scroll_pos > sec_4_y) {
-            console.log("we've reached chapter four");
-        }
-        else if (y_scroll_pos > sec_5_y) {
-            console.log("we've reached chapter five");
-        }
-
-    });
-
-*/
 
 
 
@@ -248,48 +219,7 @@ $(document).ready(function() {
 
 
 
-    // Places and repositions table of contents details on load and resize
-
-    function position_contents_details () {
-        var top = $('#chapter-1-button').offset().top;
-        $('.chapter-details').css({'top':top+25});
-
-        var ch1left = $('#chapter-1-button').offset().left;
-        $('#chapter-1-button .chapter-details').css({'left':ch1left+25});
-
-        var ch2left = $('#chapter-2-button').offset().left;
-        $('#chapter-2-button .chapter-details').css({'left':ch2left+25});
-
-        var ch3left = $('#chapter-3-button').offset().left;
-        $('#chapter-3-button .chapter-details').css({'left':ch3left+25});
-
-        var ch4left = $('#chapter-4-button').offset().left;
-        $('#chapter-4-button .chapter-details').css({'left':ch4left+25});
-
-        var ch5left = $('#chapter-5-button').offset().left;
-        $('#chapter-5-button .chapter-details').css({'left':ch5left+25});
-    }
-
-    position_contents_details();
-
-    $(window).resize(function() {
-        position_contents_details();
-    });
-
-
-
-    // Code to control rollover for table of contents buttons
-
-    var is_detail_open = false;
-    
-    $(".chapter-button").on('mouseover', function() {
-        $(this).find('.chapter-details').show();
-     })
-    .on('mouseout', function(e) {
-        $(this).find('.chapter-details').hide();
-    });
-
-
+ 
 
 
 
@@ -302,27 +232,27 @@ $('#chapter-one .year-number-lg').click(function(){
 });
 
 $('#cattle-head-yr-1').click(function(){
-    $('.cattle-head-growth img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Herd1993.png');
+    $('.ch1-map-1-holder img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Herd1993.png');
 });
 
 $('#cattle-head-yr-2').click(function(){
-    $('.cattle-head-growth img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Herd2003.png');
+    $('.ch1-map-1-holder img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Herd2003.png');
 });
 
 $('#cattle-head-yr-3').click(function(){
-    $('.cattle-head-growth img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Herd2013.png');
+    $('.ch1-map-1-holder img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Herd2013.png');
 });
 
 $('#forest-cover-yr-1').click(function(){
-    $('.forest-cover-loss img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Defor1991.png');
+    $('.ch1-map-2-holder img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Defor1991.png');
 });
 
 $('#forest-cover-yr-2').click(function(){
-    $('.forest-cover-loss img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Defor2003.png');
+    $('.ch1-map-2-holder img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Defor2003.png');
 });
 
 $('#forest-cover-yr-3').click(function(){
-    $('.forest-cover-loss img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Defor2013.png');
+    $('.ch1-map-2-holder img').attr('src','http://d2tbmhuj3dq9ke.cloudfront.net/img/Map-Defor2013.png');
 });
 
 
@@ -580,7 +510,7 @@ $('#chapter-four .year-number-lg').on('click', function(){
     var which_map = $(this).attr('id').split('-')[0];
     var ch4mapurl = "http://d2tbmhuj3dq9ke.cloudfront.net/img/"+which_map+"agreement.gif";
 
-    $('.ch4-map-overlay img').css('src',ch4mapurl);
+    $('.ch4-map-overlay img').attr('src',ch4mapurl);
 
     if (which_map == 'pre') {
         $('.ch4-box-1').hide();
@@ -610,102 +540,22 @@ $('#chapter-four .year-number-lg').on('click', function(){
 //Chapter 5 behavior
 //=====================================
 
-var which_frame = 1;
-
-function ch5_switch_frame() {
-    if (which_frame ==1) {
-        $('#ch5-image-overlay').hide();
-        $('.ch5-label-text div').hide();
-        $('#ct1').show();
-        $('#ch5-image-holder').css({
-         "background-size": "300% 300%",
-         "background-position": "left center"});
-    }
-    else if (which_frame ==2) {
-        $('#ch5-image-overlay').hide();
-        $('.ch5-label-text div').hide();
-        $('#ct2').show();
-        $('#ch5-image-holder').css({
-            "background-size": "210% 210%",
-            "background-position": "10% 55%"});
-    }
-    else if (which_frame ==3) {
-        $('#ch5-image-overlay').hide();
-        $('.ch5-label-text div').hide();
-        $('#ct3').show();
-        $('#ch5-image-holder').css({
-            "background-size": "210% 210%",
-            "background-position": "60% center"});
-    }
-    else if (which_frame ==4) {
-        $('#ch5-image-overlay').hide();
-        $('.ch5-label-text div').hide();
-        $('#ct4').show();
-        $('#ch5-image-holder').css({
-            "background-size": "175% 175%",
-            "background-position": "right 60%"});
-    }
-    else if (which_frame ==5) {
-        $('#ch5-image-overlay').hide();
-        $('.ch5-label-text div').hide();
-        $('#ct5').show();
-        $('#ch5-image-holder').css({
-            "background-size": "100% 100%"});
-    }
-    else if (which_frame ==6) {
-        $('#ch5-image-overlay').hide();
-        $('.ch5-label-text div').hide();
-        $('#ct6').show();
-        $('#ch5-image-holder').css({
-            "background-size": "100% 100%"});
-        $('#ch5-image-overlay').fadeIn(1000);
-    }
-}
-
-
-$('.ch5-left-arrow').on('click', function() {
-    if (which_frame > 1) {
-        which_frame--;
-    }
-    else {
-        which_frame = 6;
-    }
-    ch5_switch_frame();
-});
-
-
-$('.ch5-right-arrow').on('click', function() {
-    if (which_frame < 6) {
-        which_frame++;
-    }
-    else {
-        which_frame = 1;
-    }
-    ch5_switch_frame();
-});
-
-
-
 
     //Recommendations behavior
     //=====================================
 
     $('.map-point').on('click', function() {
         var which_panel = $(this).attr('id').split('open-')[1];
-        var offset_top = ($('.recommendations-map-holder').height() - $('.recommendations-panel').height())/4;
-        var offset_left = ($('.recommendations-map-holder').width() - $('.recommendations-panel').width())/4;
-        var recs_top = $('.recommendations-map-holder').offset().top+offset_top;
-        var recs_left = $('.recommendations-map-holder').offset().left+offset_left*1.5;
-        $('.recommendations-panel').css({'top': recs_top, 'left': recs_left});
-        $('.recommendations-map-holder').css('opacity', 0.4);
+        var topcoord = window.pageYOffset;
+        var panelheight = window.innerHeight*0.95;
+        $('section ,.tangent-tab-holder').css('opacity','0.3');
+        $('.recommendations-panel').css({'top': topcoord+50});
         $('#'+which_panel).fadeIn();
 
         $('.close-recs').on('click', function() {
-            $('.recommendations-map-holder').css('opacity', 1.0);
             $('.recommendations-panel').hide();
+            $('section ,.tangent-tab-holder').css('opacity','1.0');
         });
-
     });
-
 
 });
